c6aa84a4a0ac34097866dd74b56eca86bbc1a239
조무현 <jomuhyeon@jomuhyeons-MacBook-Air.local>
2024-10-07 14:41:19 +0900

    Remove tests

--- src/test/java/com/dogmeat/petconnect/petregister/PetregisterApplicationTests.java
+++ src/test/java/com/dogmeat/petconnect/petregister/PetregisterApplicationTests.java
@@ -1,13 +1,13 @@
 package com.dogmeat.petconnect.petregister;
 
-import org.junit.jupiter.api.Test;
+// import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class PetregisterApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	// @Test
+	// void contextLoads() {
+	// }
 
 }
